f15d986c29f703a7d865293fa0997154dc7db790
Reman Biswas <67569511+biswasreman@users.noreply.github.com>
2023-01-09 21:19:37 +0530

    Create ValueSwapping.py

---
+++ ValueSwapping.py
@@ -0,0 +1,7 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+x, y = y, x
+
+print("the value of x by swapping y into x we get: ", x )
+print("the value of y by swapping x into y we get: ", y)
