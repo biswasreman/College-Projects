477590a87e1f22a8183a44ba2c95b5cac058c760
Reman Biswas <67569511+biswasreman@users.noreply.github.com>
2023-01-19 20:55:47 +0530

    Add files via upload

---
+++ StringListDeleteLastTwo.py
@@ -0,0 +1,5 @@
+list = ['python','swift','c++','c','java','rust','R']
+
+list = list[:-2]
+
+print(list)
---
+++ StringListDeleteSwift.py
@@ -0,0 +1,5 @@
+list = ['python','swift','c++','c','java','rust','R']
+
+list.remove("swift")
+
+print(list)
